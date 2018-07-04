944d4d6494201dc1f89d0ea3c2c92b60d1d5feca
evandigby <evandigby@gmail.com>
2018-07-03 18:52:41 -0700

    using new md package

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gowasm/vecty"
 	"github.com/gowasm/vecty/elem"
 	"github.com/pichiw/leaflet"
+	"github.com/pichiw/md"
 )
 
 func main() {
@@ -14,91 +15,40 @@ func main() {
 	vecty.SetTitle("Pichiw")
 	vecty.AddStylesheet("app.css")
 
-	entities := []*Entity{
-		&Entity{
-			Name:  "Red River",
-			Coord: leaflet.NewCoordinate(49.8951, -97.1384),
-		},
-		&Entity{
-			Name:  "Turtle Mountain",
-			Coord: leaflet.NewCoordinate(48.8469, -99.8011),
-		},
-		&Entity{
-			Name:  "St. Paul des Metis",
-			Coord: leaflet.NewCoordinate(53.8896, -111.4657),
-		},
-		&Entity{
-			Name:  "Vancouver",
-			Coord: leaflet.NewCoordinate(49.2827, -123.1207),
-		},
-	}
-
-	var coordinates []*leaflet.Coordinate
-
-	for _, e := range entities {
-		coordinates = append(coordinates, e.Coord)
-	}
-
-	m := leaflet.NewMap(
-		"mapid",
-		leaflet.MapOptions{
-			Center:  leaflet.NewCoordinate(49.8951, -97.1384),
-			Zoom:    6,
-			MaxZoom: 18,
-		},
-		nil,
-		leaflet.NewTileLayer(
-			leaflet.TileLayerOptions{
-				MaxZoom:     18,
-				Attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`,
-			},
-		),
-	)
-
 	b := &Home{
-		m: m,
-	}
-
-	colors := []string{
-		"#ff0000",
-		"#aa2200",
-		"#66aa00",
-		"#00ff00",
-	}
-
-	for i := range entities {
-		entity := entities[i]
-
-		if i > 0 {
-			m.Add(
-				leaflet.NewPolyline(
-					leaflet.PolylineOptions{
-						PathOptions: leaflet.PathOptions{
-							Color: colors[i],
-						},
-					},
-					entities[i-1].Coord,
-					entity.Coord,
-				),
-			)
-		}
-
-		m.Add(
-			leaflet.NewMarker(
-				entity.Coord,
-				leaflet.Events{
-					"click": func(vs []js.Value) {
-						if b.e.entity == nil || b.e.entity != entity {
-							b.e.entity = entity
-							b.m.View(entity.Coord, b.m.Zoom())
-						} else {
-							b.e.entity = nil
-						}
-						vecty.Rerender(b)
-					},
+		model: []*Entity{
+			&Entity{
+				Name:  "Red River",
+				Coord: leaflet.NewCoordinate(49.8951, -97.1384),
+			},
+			&Entity{
+				Name:  "Turtle Mountain",
+				Coord: leaflet.NewCoordinate(48.8469, -99.8011),
+			},
+			&Entity{
+				Name:  "St. Paul des Metis",
+				Coord: leaflet.NewCoordinate(53.8896, -111.4657),
+			},
+			&Entity{
+				Name:  "Vancouver",
+				Coord: leaflet.NewCoordinate(49.2827, -123.1207),
+			},
+		},
+		m: leaflet.NewMap(
+			"mapid",
+			leaflet.MapOptions{
+				Center:  leaflet.NewCoordinate(49.8951, -97.1384),
+				Zoom:    6,
+				MaxZoom: 18,
+			},
+			nil,
+			leaflet.NewTileLayer(
+				leaflet.TileLayerOptions{
+					MaxZoom:     18,
+					Attribution: `&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>`,
 				},
 			),
-		)
+		),
 	}
 
 	vecty.RenderBody(b)
@@ -124,51 +74,80 @@ func (p *EntityEditor) Render() vecty.ComponentOrHTML {
 	)
 }
 
-// Home is our main page component.
 type Home struct {
 	vecty.Core
 	m *leaflet.Map
 	e EntityEditor
+
+	model []*Entity
+}
+
+func (p *Home) updateMapFromModel() {
+	colors := []string{
+		"#ff0000",
+		"#aa2200",
+		"#66aa00",
+		"#00ff00",
+	}
+
+	for i := range p.model {
+		entity := p.model[i]
+
+		if i > 0 {
+			p.m.Add(
+				leaflet.NewPolyline(
+					leaflet.PolylineOptions{
+						PathOptions: leaflet.PathOptions{
+							Color: colors[i],
+						},
+					},
+					p.model[i-1].Coord,
+					entity.Coord,
+				),
+			)
+		}
+
+		p.m.Add(
+			leaflet.NewMarker(
+				entity.Coord,
+				leaflet.Events{
+					"click": func(vs []js.Value) {
+						if p.e.entity == nil || p.e.entity != entity {
+							p.e.entity = entity
+							p.m.View(entity.Coord, p.m.Zoom())
+						} else {
+							p.e.entity = nil
+						}
+						vecty.Rerender(p)
+					},
+				},
+			),
+		)
+	}
 }
 
 // Render implements the vecty.Component interface.
 func (p *Home) Render() vecty.ComponentOrHTML {
+	p.updateMapFromModel()
 	hasElement := p.e.entity != nil
+	mapSpan := 10
+	if hasElement {
+		mapSpan = 6
+	}
 	return elem.Body(
-		elem.Div(
-			vecty.Markup(
-				vecty.Class("mdc-layout-grid"),
-			),
-			elem.Div(
-				vecty.Markup(
-					vecty.Class("mdc-layout-grid__inner"),
+		md.LayoutGrid(
+			md.LayoutGridInner(
+				md.LayoutGridCell(md.LayoutGridCellOptions{Span: 2},
+					elem.Heading1(vecty.Text("Pichiw")),
 				),
-				elem.Div(
-					vecty.Markup(
-						vecty.Class("mdc-layout-grid__cell", "mdc-layout-grid__cell--span-2"),
-					),
-					elem.Heading1(
-						vecty.Text("Pichiw"),
-					),
-				),
-				elem.Div(
-					vecty.Markup(
-						vecty.MarkupIf(hasElement,
-							vecty.Class("mdc-layout-grid__cell", "mdc-layout-grid__cell--span-6"),
-						),
-						vecty.MarkupIf(!hasElement,
-							vecty.Class("mdc-layout-grid__cell", "mdc-layout-grid__cell--span-10"),
-						),
-					),
+				md.LayoutGridCell(md.LayoutGridCellOptions{Span: mapSpan},
 					p.m,
 				),
-				vecty.If(
-					hasElement,
+				vecty.If(hasElement,
 					elem.Div(
-						vecty.Markup(
-							vecty.Class("mdc-layout-grid__cell", "mdc-layout-grid__cell--span-4"),
+						md.LayoutGridCell(md.LayoutGridCellOptions{Span: 4},
+							&p.e,
 						),
-						&p.e,
 					),
 				),
 			),
